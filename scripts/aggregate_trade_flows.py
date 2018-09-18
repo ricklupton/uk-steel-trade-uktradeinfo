eef03b87cc05935e3cce14e85afedcada88a9197
Rick Lupton <mail@ricklupton.name>
2018-09-18 20:24:40 +0100

    Update to use new sector codes and steel contents
    Now based on the ISSB sectors

--- scripts/aggregate_trade_flows.py
+++ scripts/aggregate_trade_flows.py
@@ -1,24 +1,26 @@
 import pandas as pd
 import datapackage
 
+
 def load_dataframe(filename, resource):
     """Load one table from a datapackage."""
     package = datapackage.Package(filename)
     r = package.get_resource(resource)
     return pd.DataFrame(r.read(), columns=r.headers)
 
+
 # Load the flows from the datapackage
 flows = load_dataframe('datapackage.json', 'flows')
 flows['mass'] = flows['mass'].astype(float)  # XXX this is annoying
 
 # Load the allocation table and category iron contents
-alloc = pd.read_csv('scripts/hs4_allocations.csv', usecols=['HS4', 'Allocation'], index_col='HS4')
-cats = pd.read_csv('scripts/biffaward_categories.csv', index_col=0)
+alloc = pd.read_csv('scripts/hs4_allocations.csv', usecols=['HS4', 'sector_code'], index_col='HS4')
+cats = pd.read_csv('scripts/steel_contents.csv', index_col=0)
 
 # Join the table and aggregate
 agg_total_mass = flows \
     .join(alloc, on='HS4') \
-    .groupby(['direction', 'Allocation', 'year']) \
+    .groupby(['direction', 'sector_code', 'year']) \
     .sum()
 
 # Convert kg to kt
@@ -27,13 +29,13 @@ agg_total_mass['mass'] /= 1e6
 # Add in the category iron contents
 agg_iron = agg_total_mass \
     .reset_index() \
-    .join(cats, on='Allocation')
+    .join(cats, on='sector_code')
 agg_iron['mass_iron'] = agg_iron['mass'] * agg_iron['iron_content']
 
 # Pivot into tables for viewing
 def pivot_table(direction, value):
     return agg_iron[agg_iron['direction'] == direction] \
-        .pivot(index='Allocation', columns='year', values=value)
+        .pivot(index='sector_code', columns='year', values=value)
 
 table_total_exports = pivot_table('export', 'mass')
 table_total_imports = pivot_table('import', 'mass')
@@ -41,16 +43,10 @@ table_iron_exports = pivot_table('export', 'mass_iron')
 table_iron_imports = pivot_table('import', 'mass_iron')
 
 # Save
-df = agg_iron \
-    .rename(columns={'Allocation': 'product_group_id',
-                     'name': 'product_group'}) \
-    .set_index('direction')
+df = agg_iron.set_index('direction')
 
-
-df = df[['product_group_id', 'product_group', 'year',
+df = df[['sector_code', 'sector_group', 'sector_name', 'year',
          'iron_content', 'mass', 'mass_iron']]
 
-df['product_group_id'] = df['product_group_id'].astype(int)
-
-df.loc['import'].to_csv('data/imports.csv', index=False)
-df.loc['export'].to_csv('data/exports.csv', index=False)
+df.loc['import'].to_csv('data/imports.csv', index=False, float_format='%.1f')
+df.loc['export'].to_csv('data/exports.csv', index=False, float_format='%.1f')
